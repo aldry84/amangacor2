96e29963c1a6923a763cb217469434e9674ea02d
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-10-27 01:12:39 +0900

    Update AdimovieboxProvider.kt

--- Adimoviebox/src/main/kotlin/com/Adimoviebox/AdimovieboxProvider.kt
+++ Adimoviebox/src/main/kotlin/com/Adimoviebox/AdimovieboxProvider.kt
@@ -1,1 +1,15 @@
+package com.Adimoviebox
 
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class AdimovieboxProvider: BasePlugin() {
+    override fun load() {
+        registerMainAPI(Adimoviebox())
+        // Karena Moviebox Anda menggunakan tautan stream langsung (INFER_TYPE), 
+        // dan tidak menggunakan extractor spesifik, saya tidak mendaftarkan extractor di sini.
+        // Jika link-nya adalah extractor yang terpisah (seperti dari BanglaPlex), 
+        // Anda perlu mendaftarkannya.
+    }
+}
