034fff11acb8b596239efd6b01aecefbe13c1816
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-10-27 11:38:17 +0900

    Update AdimoviemazeProvider.kt

--- Adimoviemaze/src/main/kotlin/com/Adimoviemaze/AdimoviemazeProvider.kt
+++ Adimoviemaze/src/main/kotlin/com/Adimoviemaze/AdimoviemazeProvider.kt
@@ -1,12 +1,18 @@
 package com.Adimoviemaze
 
+import com.lagradost.cloudstream3.extractors.StreamTape
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.extractors.Uqload
 
 @CloudstreamPlugin
 class AdimoviemazeProvider: BasePlugin() {
     override fun load() {
         registerMainAPI(Adimoviemaze())
-        // Daftarkan Extractor API di sini jika ada (seperti Plextream, Xcloud, dll.)
+        
+        // Beberapa extractor umum yang mungkin digunakan
+        registerExtractorAPI(StreamTape())
+        registerExtractorAPI(Uqload()) 
+        // Tambahkan extractor kustom jika Anda menemukannya di moviemaze.cc
     }
 }
