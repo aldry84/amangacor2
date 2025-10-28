af88c09e37f755081146cde6432e6ae2e4ccf520
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-10-28 15:44:12 +0900

    Create AdidrakorProvider.kt

---
+++ AdiDrakor/src/main/kotlin/com/AdiDrakor/AdidrakorProvider.kt
@@ -0,0 +1,12 @@
+package com.AdiDrakor
+
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class AdidrakorProvider: BasePlugin() {
+    override fun load() {
+        registerMainAPI(AdiDrakor())
+        // Tidak ada extractor tambahan yang didaftarkan.
+    }
+}
