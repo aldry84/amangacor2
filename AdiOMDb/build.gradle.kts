d3c6bea70d613e766e588f5ac38bd165ae889fc4
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-10-30 01:31:24 +0900

    Update build.gradle.kts

--- AdiOMDb/build.gradle.kts
+++ AdiOMDb/build.gradle.kts
@@ -1,55 +1,28 @@
-// --- BLOK WAJIB UNTUK GRADLE/ANDROID ---
-plugins {
-    id("com.android.library")
-    id("kotlin-android")
-    id("kotlin-kapt")
-    // Plugin utama Cloudstream3
-    id("com.lagradost.cloudstream3")
-}
-
-// Tambahkan definisi repositori agar Gradle dapat menemukan dependensi
-repositories {
-    google()
-    mavenCentral()
-}
-
-// Konfigurasi Android (Wajib ada)
-android {
-    namespace = "com.AdiOMDb" // Menggunakan namespace AdiOMDb yang benar
-    compileSdk = 34
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0" 
-    }
-}
-// --- AKHIR BLOK WAJIB ---
-
-
 // use an integer for version numbers
 version = 1
+
 
 cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    description = "AdiOMDb (Metadata OMDb dan Streaming Fmoviesunblocked.net)" 
-    language    = "en" 
-    authors = listOf("AdiOMDbUser") 
+    description = "AdiOMDb (OMDb)" // Deskripsi yang diperbarui
+    language    = "en" // Bahasa dari OMDb
+    authors = listOf("AdiOMDb") // Ganti sesuai keinginan Anda
 
-    status = 1 
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 // will be 3 if unspecified
 
-    tvTypes = listOf("TvSeries", "AsianDrama", "Movie")
+    // List of video source types. Users are able to filter for extensions in a given category.
+    // Anda mendukung semua tipe dari Moviebox
+    tvTypes = listOf("Movie","TvSeries","Anime","AsianDrama")
 
-    iconUrl="https://www.google.com/s2/favicons?domain=fmoviesunblocked.net&sz=%size%"
+    iconUrl="https://www.google.com/s2/favicons?domain=moviebox.ph&sz=%size%"
 
     isCrossPlatform = true
-}
-
-// Dependensi yang diperlukan (Wajib ada)
-dependencies {
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation(project(":lib:cloudstream-impl"))
 }
