78e9d87bd7904f2823ee9663b293d66be981407e
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-10-30 01:04:07 +0900

    Update build.gradle.kts

--- AdiOMDb/build.gradle.kts
+++ AdiOMDb/build.gradle.kts
@@ -1,68 +1,37 @@
-// --- BLOK WAJIB: DEFINISI PLUGINS DAN REPOSITORI ---
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        // Mendefinisikan versi plugin yang diperlukan di buildscript
-        classpath("com.lagradost.cloudstream3:cloudstream-plugin:1.7.0") 
-        classpath("com.android.tools.build:gradle:8.2.0") // Android Gradle Plugin (AGP)
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0") // Kotlin Plugin
-    }
-}
+apply plugin: 'com.android.library'
+apply plugin: 'kotlin-android'
+apply plugin: 'kotlin-kapt'
+apply plugin: 'com.lagradost.cloudstream3'
 
-// Mengaplikasikan Plugin (Mengganti Blok 'plugins' dengan 'apply')
-apply(plugin = "com.android.library")
-apply(plugin = "kotlin-android")
-apply(plugin = "kotlin-kapt")
-apply(plugin = "com.lagradost.cloudstream3")
-
-
-// Tambahkan definisi repositori untuk dependensi utama project
-repositories {
-    google()
-    mavenCentral()
-}
-
-// Konfigurasi Android
 android {
-    namespace = "com.AdiOMDb"
-    compileSdk = 34
+    compileSdkVersion 34
     defaultConfig {
-        minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0" 
+        minSdkVersion 21
+        targetSdkVersion 34
+        versionCode 1
+        versionName "1.0.0" 
     }
+    buildTypes {
+        release {
+            minifyEnabled false
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        }
+    }
+    namespace 'com.AdiOMDb'
 }
-
-// --- METADATA CLOUDSTREAM (SESUAI PERMINTAAN ANDA) ---
-
-// use an integer for version numbers
-version = 1
 
 cloudstream {
-    description = "AdiOMDb: Metadata OMDb dan Streaming Fmoviesunblocked" 
-    language    = "en"
-    authors = listOf("AdiOMDbUser") 
-
-    status = 1 
-
-    // Menggunakan tipe umum yang kompatibel dengan OMDb
-    tvTypes = listOf("TvSeries", "Movie") 
-
-    // Menggunakan domain OMDb sebagai sumber ikon utama
-    iconUrl="https://www.google.com/s2/favicons?domain=omdbapi.com&sz=%size%"
-
-    isCrossPlatform = true
+    // ID Unik Plugin Anda
+    url = 'AdiOMDb.com' 
+    // Nama Provider yang akan ditampilkan di Cloudstream3
+    desc = 'Menggabungkan Metadata OMDb dan Streaming Fmoviesunblocked'
+    authors = listOf("Adi")
 }
 
-// Dependensi yang diperlukan
 dependencies {
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    // Ini mengasumsikan folder proyek Anda memiliki modul ':lib:cloudstream-impl'
-    implementation(project(":lib:cloudstream-impl")) 
+    implementation fileTree(dir: 'libs', include: ['*.jar'])
+    implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version"
+    implementation "androidx.core:core-ktx:1.12.0"
+    implementation "androidx.appcompat:appcompat:1.6.1"
+    implementation project(':lib:cloudstream-impl')
 }
