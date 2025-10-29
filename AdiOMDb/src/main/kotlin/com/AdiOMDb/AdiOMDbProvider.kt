69db81cc97e7fc2e0127975dc75eb00517720484
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-10-30 02:10:15 +0900

    Update AdiOMDbProvider.kt

--- AdiOMDb/src/main/kotlin/com/AdiOMDb/AdiOMDbProvider.kt
+++ AdiOMDb/src/main/kotlin/com/AdiOMDb/AdiOMDbProvider.kt
@@ -1,12 +1,11 @@
 package com.AdiOMDb
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.PluginRepository
-import android.content.Context
+import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class AdiOMDbProvider: PluginRepository() {
-    override fun load(context: Context) {
+class AdiOMDbProvider: Plugin() { // Ganti PluginRepository menjadi Plugin()
+    override fun load() { // Hapus parameter 'context' dan kata kunci 'override' pada 'load' (atau pastikan 'load()' adalah fungsi yang benar untuk di-override)
         // Mendaftarkan MainAPI
         registerMainAPI(AdiOMDb())
     }
