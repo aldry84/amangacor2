61d5536a952b7c3d841710004dadfdebfed281e3
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-10-30 16:02:10 +0900

    Update AdiOMDbProvider.kt

--- AdiOMDb/src/main/kotlin/com/AdiOMDb/AdiOMDbProvider.kt
+++ AdiOMDb/src/main/kotlin/com/AdiOMDb/AdiOMDbProvider.kt
@@ -1,13 +1,13 @@
 package com.AdiOMDb
 
-import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin 
-import com.lagradost.cloudstream3.plugins.Plugin // Import ini seharusnya com.lagradost.cloudstream3.plugins.Plugin jika di versi Cloudstream yang lebih baru
+import com.lagradost.cloudstream3.plugins.Plugin // Class dasar yang digunakan untuk pendaftaran
 
 @CloudstreamPlugin
-class AdiOMDbProvider: BasePlugin() { // Mengubah Plugin() menjadi BasePlugin() jika menggunakan Cloudstream 4.x
-    // Menggunakan BasePlugin() untuk konsistensi dengan versi Cloudstream yang lebih baru
+class AdiOMDbProvider: Plugin() { 
+    // Pastikan Anda menggunakan fungsi 'registerMainAPI' di dalam 'load'
     override fun load() { 
+        // Logika sederhana untuk mendaftarkan API utama Anda
         registerMainAPI(AdiOMDb())
     }
 }
