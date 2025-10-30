008f08b9a88e3f49f18a34ea9fd864dbcde333bf
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-10-30 18:25:11 +0900

    Update AdiOMDbProvider.kt

--- AdiOMDb/src/main/kotlin/com/AdiOMDb/AdiOMDbProvider.kt
+++ AdiOMDb/src/main/kotlin/com/AdiOMDb/AdiOMDbProvider.kt
@@ -3,10 +3,10 @@ package com.AdiOMDb
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
-// MENGGUNAKAN BasePlugin SEPERTI PADA Adimoviebox
 @CloudstreamPlugin
-class AdiOMDbProvider: BasePlugin() { 
+class AdiOMDbProvider : BasePlugin() {
     override fun load() {
+        // Daftarkan sumber utama ke Cloudstream
         registerMainAPI(AdiOMDb())
     }
 }
