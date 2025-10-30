245cb9e18dd1dc0cbeb0d607762de1b4ab16883f
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-10-30 15:47:00 +0900

    Update AdiOMDbProvider.kt

--- AdiOMDb/src/main/kotlin/com/AdiOMDb/AdiOMDbProvider.kt
+++ AdiOMDb/src/main/kotlin/com/AdiOMDb/AdiOMDbProvider.kt
@@ -2,17 +2,11 @@ package com.AdiOMDb
 
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin 
+import com.lagradost.cloudstream3.plugins.Plugin // Import ini seharusnya com.lagradost.cloudstream3.plugins.Plugin jika di versi Cloudstream yang lebih baru
 
 @CloudstreamPlugin
-class AdiOMDbProvider: Plugin() { 
-    // Jika 'override fun load()' gagal, coba load dengan Context sebagai fallback untuk versi lama:
-    /*
-    override fun load(context: Context) { 
-        registerMainAPI(AdiOMDb())
-    }
-    */
-    
-    // Namun, yang paling stabil saat ini di Cloudstream 4.x adalah:
+class AdiOMDbProvider: BasePlugin() { // Mengubah Plugin() menjadi BasePlugin() jika menggunakan Cloudstream 4.x
+    // Menggunakan BasePlugin() untuk konsistensi dengan versi Cloudstream yang lebih baru
     override fun load() { 
         registerMainAPI(AdiOMDb())
     }
