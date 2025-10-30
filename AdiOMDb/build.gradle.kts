fcc57883591bcad41c1a268cd43e23f8c8e54c5a
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-10-30 18:28:10 +0900

    Update build.gradle.kts

--- AdiOMDb/build.gradle.kts
+++ AdiOMDb/build.gradle.kts
@@ -1,29 +1,28 @@
 // AdiOMDb/build.gradle.kts
 
-// use an integer for version numbers
-version = 5
-
+version = 6 // Naikkan jika update plugin
 
 cloudstream {
-    // All of these properties are optional, you can safely remove them
-
-    description = "AdiOMDb (OMDb)" 
-    language    = "en" 
-    authors = listOf("AdiOMDb") 
+    description = "Hybrid OMDb metadata and FMovies streaming provider"
+    language = "en"
+    authors = listOf("AdiOMDb")
 
     /**
-    * Status int as the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta only
-    * */
-    status = 1 
+     * Status:
+     * 0 = Down
+     * 1 = Working
+     * 2 = Slow
+     * 3 = Beta/Test only
+     */
+    status = 1
 
-    // List of video source types.
-    tvTypes = listOf("Movie","TvSeries","Anime","AsianDrama")
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime",
+        "AsianDrama"
+    )
 
-    iconUrl="https://www.google.com/s2/favicons?domain=moviebox.ph&sz=%size%"
-
+    iconUrl = "https://www.google.com/s2/favicons?domain=omdbapi.com&sz=%size%"
     isCrossPlatform = true
 }
