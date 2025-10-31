c7477e463aa1c0355a4b89460ddf57eaf84244ed
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-10-31 17:29:33 +0900

    Update MyTMDBProviderPlugin.kt

--- MyTMDBProvider.kt/src/main/kotlin/com/mytmdbprovider/MyTMDBProviderPlugin.kt
+++ MyTMDBProvider.kt/src/main/kotlin/com/mytmdbprovider/MyTMDBProviderPlugin.kt
@@ -1,14 +1,12 @@
 package mytmdbprovider 
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.APIHolder.Companion.registerMainAPI // Mengganti registerMainPageSource
-import android.content.Context // Diperlukan untuk fungsi load()
+import com.lagradost.cloudstream3.APIHolder.Companion.registerMainAPI
+import android.content.Context 
 
 // Kelas ini adalah TITIK MASUK (entry point) ekstensi Anda
 class MyTMDBProviderPlugin: CloudstreamPlugin() { 
-    // Fungsi load() Cloudstream memerlukan parameter context
     override fun load(context: Context) { 
-        // Daftarkan Source utama Anda
         registerMainAPI(MyTMDBProvider()) 
     }
 }
