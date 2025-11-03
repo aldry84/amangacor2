20447035d92f89681cc68ceea03800c95a88fe59
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-11-03 14:31:58 +0900

    Update AdicinemaxNewPlugin.kt

--- AdicinemaxNew/src/main/kotlin/com/AdicinemaxNew/AdicinemaxNewPlugin.kt
+++ AdicinemaxNew/src/main/kotlin/com/AdicinemaxNew/AdicinemaxNewPlugin.kt
@@ -6,7 +6,7 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 @CloudstreamPlugin
 class AdicinemaxNewPlugin: BasePlugin() {
     override fun load() {
-        registerMainAPI(Adicinemax())
+        registerMainAPI(AdicinemaxNew())
         // Karena Moviebox Anda menggunakan tautan stream langsung (INFER_TYPE), 
         // dan tidak menggunakan extractor spesifik, saya tidak mendaftarkan extractor di sini.
         // Jika link-nya adalah extractor yang terpisah (seperti dari BanglaPlex), 
