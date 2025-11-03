1f2c5f03b0ea678370c76c8722fb0294bd977f3c
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-11-03 14:16:04 +0900

    Update AdicinemaxNewPlugin.kt

--- AdicinemaxNew/src/main/kotlin/com/AdicinemaxNew/AdicinemaxNewPlugin.kt
+++ AdicinemaxNew/src/main/kotlin/com/AdicinemaxNew/AdicinemaxNewPlugin.kt
@@ -2,12 +2,14 @@ package com.AdicinemaxNew
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-// HAPUS import android.content.Context (Karena ini adalah import Android)
+// Hapus import android.content.Context
 
 @CloudstreamPlugin
 class AdicinemaxNewPlugin: Plugin() {
-    // GANTI 'Context' dengan 'Any'
-    override fun load(context: Any) {
+    
+    // Menggunakan load() tanpa parameter: Ini adalah signature yang valid
+    // untuk plugin cross-platform/non-Android.
+    override fun load() {
         registerMainAPI(Adicinemax()) 
     }
 }
