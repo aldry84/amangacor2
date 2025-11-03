04631d405a4ad0d0572c0b9d7a79a71c2e4f7818
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-11-03 13:59:19 +0900

    Update build.gradle.kts

--- AdicinemaxNew/build.gradle.kts
+++ AdicinemaxNew/build.gradle.kts
@@ -1,23 +1,31 @@
 // use an integer for version numbers
-version = 4
+version = 5
+
 
 cloudstream {
-    language = "en"
-    
-    // KITA KEMBALI KE SINTAKSIS INI: 
-    packageName = "com.AdicinemaxNew" 
+    // All of these properties are optional, you can safely remove them
 
-    description = "Metadata dan Streaming dari Adicinemax" 
-    authors = listOf("Hexated", "AdicinemaxCreator") 
+    description = "Film Terbaru"
+    authors = listOf("AdicinemaxNew")
 
-    status = 1 
-    
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 // will be 3 if unspecified
     tvTypes = listOf(
+        "AsianDrama",
         "TvSeries",
         "Movie",
-        "Anime",
-        "AsianDrama",
     )
 
-    iconUrl = "https://example.com/adicinemax_icon.png" 
+    requiresResources = true
+    language = "en"
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=dramafull.cc&sz=%size%"
+
+    isCrossPlatform = true
 }
