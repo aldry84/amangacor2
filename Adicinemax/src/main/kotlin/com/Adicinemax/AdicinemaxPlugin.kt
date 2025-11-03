fe192527c335b6aa27109800345faa79019d18e3
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-11-03 13:20:02 +0900

    Update AdicinemaxPlugin.kt

--- Adicinemax/src/main/kotlin/com/Adicinemax/AdicinemaxPlugin.kt
+++ Adicinemax/src/main/kotlin/com/Adicinemax/AdicinemaxPlugin.kt
@@ -1,13 +1,12 @@
 package com.Adicinemax
 
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin // Harus ada
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
 class AdicinemaxPlugin: Plugin() {
     override fun load(context: Context) {
-        // Mendaftarkan kelas Adicinemax
-        registerMainAPI(Adicinemax()) 
+        registerMainAPI(Adicinemax())
     }
 }
