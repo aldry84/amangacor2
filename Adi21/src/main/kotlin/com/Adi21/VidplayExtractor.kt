2171712f67f97a8f25cd3b08015f7378ab60c01e
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-11-03 21:32:57 +0900

    Create VidplayExtractor.kt

---
+++ Adi21/src/main/kotlin/com/Adi21/VidplayExtractor.kt
@@ -0,0 +1,26 @@
+package com.Adi21
+
+import com.lagradost.cloudstream3.extractors.*
+import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.*
+
+class VidplayExtractor : ExtractorApi() {
+    override val name = "Vidplay"
+    override val mainUrl = "https://vidplay.to"
+    override val requiresReferer = false
+
+    override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink> {
+        val doc = app.get(url).document
+        val videoUrl = doc.select("video source").attr("src")
+        return listOf(
+            newExtractorLink(
+                source = name,
+                name = "Vidplay",
+                url = videoUrl,
+                referer = url,
+                quality = 720,
+                isM3u8 = videoUrl.endsWith(".m3u8")
+            )
+        )
+    }
+}
