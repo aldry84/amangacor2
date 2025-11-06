4db4f600a82ad734c766e1e79f321852fe5b7a9d
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-11-07 00:22:00 +0900

    Create build.gradle.kts

---
+++ Movie21/build.gradle.kts
@@ -0,0 +1,12 @@
+// use an integer for version numbers
+version = 1
+
+cloudstream {
+    description = "Movie21 - Metadata dari TMDb, Streaming dari API Kustom" 
+    language    = "id"
+    authors = listOf("Phisher98") 
+    status = 1 
+    tvTypes = listOf("Movie","TvSeries","AsianDrama")
+    iconUrl="https://www.google.com/s2/favicons?domain=themoviedb.org&sz=%size%" // Menggunakan ikon TMDb atau kustom
+    isCrossPlatform = true
+}
