82135c71338d316116b2d415bc11e5aa3a0ea12f
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-11-07 01:57:30 +0900

    Update VidsrcEmbedExtractor.kt

--- Movie21/src/main/kotlin/com/Phisher98/VidsrcEmbedExtractor.kt
+++ Movie21/src/main/kotlin/com/Phisher98/VidsrcEmbedExtractor.kt
@@ -1,34 +1,34 @@
-// File: com.Phisher98.VidsrcEmbedExtractor.kt
 package com.Phisher98
 
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.ExtractorApi // Pastikan ini di-import
+import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.utils.loadExtractor
-import com.lagradost.cloudstream3.app // PERBAIKAN KRITIS: Import 'app' untuk memanggil app.get()
+import com.lagradost.cloudstream3.app // KRITIS: Diperlukan untuk memanggil app.get()
 
 class VidsrcEmbedExtractor : ExtractorApi() {
     override val name = "VidsrcEmbed"
     override val mainUrl = "https://vidsrc-embed.ru" 
     override val requiresReferer = true
     
-    // PERBAIKAN: Mengganti nama fungsi agar sesuai dengan base class ExtractorApi
+    // PERBAIKAN: Fungsi getLinks yang di-override dengan benar
     override suspend fun getLinks(
         url: String, 
         referer: String,
         callback: (ExtractorLink) -> Unit
     ): Boolean {
-        // PERBAIKAN: app.get() kini teratasi berkat import com.lagradost.cloudstream3.app
+        // Mendapatkan halaman embed
         val page = app.get(url, headers = mapOf("referer" to referer)) 
 
+        // Mencari URL dari iframe player yang biasanya menunjuk ke host video
         val iframeSrc = page.document.select("iframe[src*=\"embed\"]").attr("src")
 
         if (iframeSrc.isNullOrBlank()) {
             return false
         }
         
-        // PERBAIKAN: Panggilan loadExtractor kini memiliki semua argumen yang benar
+        // Memanggil loadExtractor lagi dengan URL host internal yang ditemukan
         loadExtractor(iframeSrc, url, callback) 
         
         return true
