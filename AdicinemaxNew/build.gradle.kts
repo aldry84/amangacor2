7d12eff6b3b5b2e35eaff690279c891927889b18
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-11-08 15:26:41 +0900

    Create build.gradle.kts

---
+++ AdicinemaxNew/build.gradle.kts
@@ -0,0 +1,38 @@
+// use an integer for version numbers
+version = 1
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    description = "Movies and TV Shows with TMDB metadata and VidSrc streaming"
+    language = "en"
+    authors = listOf("AdicinemaxNew")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 // will be 3 if unspecified
+
+    // List of video source types. Users are able to filter for extensions in a given category.
+    // You can find a list of available types here:
+    // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
+    iconUrl = "https://www.google.com/s2/favicons?domain=vidsrc-embed.ru&sz=%size%"
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+        "Anime",
+        "AsianDrama"
+    )
+
+    isCrossPlatform = false
+}
+
+dependencies {
+    // FIXME remove this when crossplatform is fully supported
+    val cloudstream by configurations
+    cloudstream("com.lagradost:cloudstream3:pre-release")
+}
