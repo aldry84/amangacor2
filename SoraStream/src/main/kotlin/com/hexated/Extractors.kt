3d426dda59f3a7679559e9137d3fddcdb13cb362
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-11-13 19:57:33 +0900

    Update Extractors.kt

--- SoraStream/src/main/kotlin/com/hexated/Extractors.kt
+++ SoraStream/src/main/kotlin/com/hexated/Extractors.kt
@@ -9,6 +9,7 @@ import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.getAndUnpack
 import com.lagradost.cloudstream3.utils.newExtractorLink
+import com.lagradost.cloudstream3.utils.newSubtitleFile // Import yang diperlukan
 
 open class Jeniusplay2 : ExtractorApi() {
     override val name = "Jeniusplay"
@@ -49,7 +50,7 @@ open class Jeniusplay2 : ExtractorApi() {
                     getAndUnpack(script.data()).substringAfter("\"tracks\":[").substringBefore("],")
                 tryParseJson<List<Tracks>>("[$subData]")?.map { subtitle ->
                     subtitleCallback.invoke(
-                        SubtitleFile(
+                        newSubtitleFile( // Perbaikan: Menggunakan newSubtitleFile
                             getLanguage(subtitle.label ?: ""),
                             subtitle.file
                         )
