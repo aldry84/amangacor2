e46de822582d362510d3bdc5b8861f645ba5d796
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-11-16 15:15:22 +0900

    Update AdiDrakorProviderPlugin.kt

--- AdiDrakor/src/main/kotlin/com/AdiDrakor/AdiDrakorProviderPlugin.kt
+++ AdiDrakor/src/main/kotlin/com/AdiDrakor/AdiDrakorProviderPlugin.kt
@@ -4,9 +4,9 @@ import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
 @CloudstreamPlugin
-class AdiDrakorProviderPlugin: BasePlugin() {
+class KisskhProviderPlugin: BasePlugin() {
     override fun load() {
         // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(AdiDrakorProvider())
+        registerMainAPI(KisskhProvider())
     }
 }
