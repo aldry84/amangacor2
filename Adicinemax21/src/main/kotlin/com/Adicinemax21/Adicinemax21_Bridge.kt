180fbeeb82ba1e0aa7e53a78e7e8d87852d06ac1
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-11-16 10:13:16 +0900

    Update Adicinemax21_Bridge.kt

--- Adicinemax21/src/main/kotlin/com/Adicinemax21/Adicinemax21_Bridge.kt
+++ Adicinemax21/src/main/kotlin/com/Adicinemax21/Adicinemax21_Bridge.kt
@@ -1,11 +1,7 @@
 package com.Adicinemax21
 
-// Ini adalah file bridge untuk memanggil fungsi-fungsi dari StreamPlayExtractor
-// yang masih berada di package com.phisher98
+// --- IMPORT DARI PACKAGE ASAL (com.phisher98) ---
 import com.phisher98.StreamPlayExtractor
-
-// Karena StreamPlayExtractor adalah object, kita bisa langsung menggunakannya.
-// Tidak perlu isi di sini, cukup mendeklarasikan import agar bisa diakses oleh Adicinemax21.kt
 
 /**
  * Object bridge untuk mengakses StreamPlayExtractor dari package com.phisher98.
