6d4a08e61d36fdc139f4e8289fc214dbe4f62ed1
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-11-16 13:50:28 +0900

    Create build.gradle.kts

---
+++ AdiDrakor/build.gradle.kts
@@ -0,0 +1,44 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+// use an integer for version numbers
+version = 16
+
+android {
+    defaultConfig {
+        val properties = Properties()
+        properties.load(project.rootProject.file("local.properties").inputStream())
+        android.buildFeatures.buildConfig=true
+        buildConfigField("String", "AdiDrakor", "\"${properties.getProperty("AdiDrakor")}\"")
+        buildConfigField("String", "AdiDrakorSub", "\"${properties.getProperty("AdiDrakorSub")}\"")
+
+    }
+}
+
+
+cloudstream {
+    language = "en"
+    // All of these properties are optional, you can safely remove them
+
+    // description = "Lorem Ipsum"
+    
+ authors = listOf("Phisher98,Hexated,Peerless")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "AsianDrama",
+        "TvSeries",
+        "Anime",
+      
+  "Movie",
+    )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=adidrakor.co&sz=%size%"
+
+    isCrossPlatform = true
+}
