b69b6a46c777579693ad9416e67adbe088a971b8
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-11-19 14:59:40 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,39 +1,3 @@
-// =======================================================
-// BAGIAN INI UNTUK MEMPERBAIKI ERROR KOMPILASI ANDA
-// =======================================================
-
-pluginManagement {
-    repositories {
-        // Repositori tempat plugin build CloudStream disimpan
-        maven("https://jitpack.io")
-        
-        // Repositori standar Gradle
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-    resolutionStrategy {
-        eachPlugin {
-            // INI PERBAIKANNYA: 
-            // Memberi tahu Gradle bahwa "com.github.recloudstream.gradle"
-            // sebenarnya adalah "cs-gradle"
-            if (requested.id.id == "com.github.recloudstream.gradle") {
-                useModule("com.github.recloudstream:cs-gradle:${requested.version}")
-            }
-        }
-    }
-}
-
-plugins {
-    // Menggunakan versi stabil BUKAN "master-SNAPSHOT"
-    id("com.github.recloudstream.gradle") version "1.2.2" apply false
-}
-
-
-// =======================================================
-// KODE ASLI ANDA DIMULAI DARI SINI (INI SUDAH BENAR)
-// =======================================================
-
 rootProject.name = "CloudstreamPlugins"
 
 // This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
