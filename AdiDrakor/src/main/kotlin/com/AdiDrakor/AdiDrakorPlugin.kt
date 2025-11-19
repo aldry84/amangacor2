fe25c07577d583542c39e7260a7ad78444513c3a
aldry84 <aldrymanusiwa.aman@gmail.com>
2025-11-20 01:03:49 +0900

    Update AdiDrakorPlugin.kt

--- AdiDrakor/src/main/kotlin/com/AdiDrakor/AdiDrakorPlugin.kt
+++ AdiDrakor/src/main/kotlin/com/AdiDrakor/AdiDrakorPlugin.kt
@@ -1,4 +1,5 @@
 package com.AdiDrakor
+
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
@@ -6,6 +7,8 @@ import android.content.Context
 @CloudstreamPlugin
 class AdiDrakorPlugin : Plugin() {
     override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(AdiDrakor())
+        registerExtractorAPI(Jeniusplay2())
     }
 }
