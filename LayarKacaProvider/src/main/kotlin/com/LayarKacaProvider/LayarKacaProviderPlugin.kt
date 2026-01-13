21c3f5b5eb6bac65289db5c0773aa841352977e8
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-13 15:39:14 +0900

    Update LayarKacaProviderPlugin.kt

--- LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
+++ LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
@@ -1,29 +1,31 @@
 package com.layarKacaProvider
 
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.extractors.VidHidePro6
 import com.lagradost.cloudstream3.extractors.MixDrop
 import com.lagradost.cloudstream3.extractors.StreamTape
 import com.lagradost.cloudstream3.extractors.DoodLaExtractor
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.BasePlugin
 
 @CloudstreamPlugin
 class LayarKacaProviderPlugin: BasePlugin() {
     override fun load() {
         registerMainAPI(LayarKacaProvider())
         
-        // Extractor Khusus (Milikmu)
+        // Extractor Utama
         registerExtractorAPI(Hownetwork())
         registerExtractorAPI(Cloudhownetwork())
-        registerExtractorAPI(F16px())             // Untuk CAST
-        registerExtractorAPI(EmturbovidCustom())  // Untuk TURBOVIP
-        registerExtractorAPI(Turbovidhls())
+        registerExtractorAPI(Turbovidhls())       // Pastikan ini terdaftar
+        registerExtractorAPI(EmturbovidCustom())  // Untuk redirect Turbo
+        registerExtractorAPI(F16px())             // Untuk Cast
         
         // Extractor Cadangan
         registerExtractorAPI(Furher())
         registerExtractorAPI(Furher2())
         registerExtractorAPI(Co4nxtrl())
 
-        // Extractor Bawaan (Generic)
+        // Extractor Bawaan Cloudstream
+        registerExtractorAPI(VidHidePro6())
         registerExtractorAPI(MixDrop())    
         registerExtractorAPI(StreamTape())
         registerExtractorAPI(DoodLaExtractor())
