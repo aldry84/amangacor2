2aea7483eee53e6274f73e0480fc3df1722e33b8
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-13 13:47:54 +0900

    Update LayarKacaProviderPlugin.kt

--- LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
+++ LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
@@ -2,21 +2,33 @@ package com.layarKacaProvider
 
 import com.lagradost.cloudstream3.extractors.EmturbovidExtractor
 import com.lagradost.cloudstream3.extractors.VidHidePro6
+import com.lagradost.cloudstream3.extractors.GenericExtractor
+import com.lagradost.cloudstream3.extractors.Hydrax
+import com.lagradost.cloudstream3.extractors.MixDrop
+import com.lagradost.cloudstream3.extractors.StreamTape
+import com.lagradost.cloudstream3.extractors.DoodLaExtractor
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.BasePlugin
 
 @CloudstreamPlugin
 class LayarKacaProviderPlugin: BasePlugin() {
     override fun load() {
-        // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(LayarKacaProvider())
-        registerExtractorAPI(EmturbovidExtractor())
+        
+        // Custom Extractors (Milikmu)
         registerExtractorAPI(Furher())
         registerExtractorAPI(Hownetwork())
-        registerExtractorAPI(VidHidePro6())
         registerExtractorAPI(Furher2())
         registerExtractorAPI(Turbovidhls())
-        registerExtractorAPI(Cloudhownetwork())
+        registerExtractorAPI(Cloudhownetwork()) // Ini yang akan menangani TURBOVIP (via cloud.hownetwork.xyz)
         registerExtractorAPI(Co4nxtrl())
+
+        // Generic/Built-in Extractors (PENTING sebagai cadangan)
+        registerExtractorAPI(EmturbovidExtractor())
+        registerExtractorAPI(VidHidePro6())
+        registerExtractorAPI(Hydrax())     
+        registerExtractorAPI(MixDrop())    
+        registerExtractorAPI(StreamTape())
+        registerExtractorAPI(DoodLaExtractor())
     }
 }
