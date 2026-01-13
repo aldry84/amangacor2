1b894da317bed63f85e0cbef13d95fbe1fb9e207
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-13 23:08:28 +0900

    Update LayarKacaProviderPlugin.kt

--- LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
+++ LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
@@ -1,9 +1,6 @@
 package com.layarKacaProvider
 
 import com.lagradost.cloudstream3.extractors.VidHidePro6
-import com.lagradost.cloudstream3.extractors.MixDrop
-import com.lagradost.cloudstream3.extractors.StreamTape
-import com.lagradost.cloudstream3.extractors.DoodLaExtractor
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.BasePlugin
 
@@ -12,26 +9,19 @@ class LayarKacaProviderPlugin: BasePlugin() {
     override fun load() {
         registerMainAPI(LayarKacaProvider())
         
-        // PENTING: Daftarkan ini paling atas
-        registerExtractorAPI(PlayerIframe()) 
+        // --- ADDED ---
+        registerExtractorAPI(PlayerIframe())
         registerExtractorAPI(UniversalVIP())
-        
-        // P2P
-        registerExtractorAPI(Hownetwork())
-        registerExtractorAPI(Cloudhownetwork())
-        
-        // Redirectors
-        registerExtractorAPI(Turbovidhls())
         registerExtractorAPI(F16px())
         registerExtractorAPI(EmturbovidCustom())
         
-        // Cadangan
+        // --- ORIGINAL ---
+        registerExtractorAPI(Hownetwork())
+        registerExtractorAPI(VidHidePro6())
         registerExtractorAPI(Furher())
         registerExtractorAPI(Furher2())
+        registerExtractorAPI(Turbovidhls())
+        registerExtractorAPI(Cloudhownetwork())
         registerExtractorAPI(Co4nxtrl())
-        registerExtractorAPI(VidHidePro6())
-        registerExtractorAPI(MixDrop())    
-        registerExtractorAPI(StreamTape())
-        registerExtractorAPI(DoodLaExtractor())
     }
 }
