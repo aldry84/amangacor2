aba7c14804f1ea6d8b84234e813b86ae9731ba06
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-13 14:07:18 +0900

    Update LayarKacaProviderPlugin.kt

--- LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
+++ LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
@@ -1,34 +1,31 @@
 package com.layarKacaProvider
 
-import com.lagradost.cloudstream3.extractors.EmturbovidExtractor
-import com.lagradost.cloudstream3.extractors.VidHidePro6
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.extractors.MixDrop
 import com.lagradost.cloudstream3.extractors.StreamTape
 import com.lagradost.cloudstream3.extractors.DoodLaExtractor
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.BasePlugin
 
 @CloudstreamPlugin
 class LayarKacaProviderPlugin: BasePlugin() {
     override fun load() {
         registerMainAPI(LayarKacaProvider())
         
-        // Custom Extractors (Milikmu)
-        registerExtractorAPI(Furher())
+        // Extractor Khusus yang kita buat di Extractors.kt
         registerExtractorAPI(Hownetwork())
+        registerExtractorAPI(Cloudhownetwork())
+        registerExtractorAPI(F16px())             // Untuk CAST / VidHide
+        registerExtractorAPI(EmturbovidCustom())  // Untuk TURBOVIP
+        
+        // Extractor Cadangan
+        registerExtractorAPI(Furher())
         registerExtractorAPI(Furher2())
         registerExtractorAPI(Turbovidhls())
-        registerExtractorAPI(Cloudhownetwork())
         registerExtractorAPI(Co4nxtrl())
 
-        // Generic/Built-in Extractors (PENTING)
-        registerExtractorAPI(EmturbovidExtractor())
-        registerExtractorAPI(VidHidePro6())
+        // Extractor Bawaan (Generic)
         registerExtractorAPI(MixDrop())    
         registerExtractorAPI(StreamTape())
         registerExtractorAPI(DoodLaExtractor())
-        
-        // Catatan: Hydrax dan GenericExtractor dihapus sementara agar bisa build.
-        // Deteksi link tetap akan berjalan via Regex di Provider.
     }
 }
