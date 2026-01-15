613e1a5e37967de95413dacd586aea9bd26bfd76
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-16 06:36:11 +0900

    Update HomeSpeciallyPlugin.kt

--- IndoFilm/src/main/kotlin/com/IndoFilm/HomeSpeciallyPlugin.kt
+++ IndoFilm/src/main/kotlin/com/IndoFilm/HomeSpeciallyPlugin.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class HomeSpeciallyPlugin: CloudstreamPlugin() {
+class HomeSpeciallyPlugin: Plugin() { // <--- GANTI INI (Dulu: CloudstreamPlugin)
     override fun load(context: Context) {
         // Mendaftarkan class utama provider kita
         registerMainAPI(HomeSpecially())
