1241c4024237ebe0ed21014689fb4df6ed6d9eb5
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-15 17:04:11 +0900

    Update LayarKacaProviderPlugin.kt

--- LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
+++ LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
@@ -7,18 +7,20 @@ import com.lagradost.cloudstream3.plugins.BasePlugin
 @CloudstreamPlugin
 class LayarKacaProviderPlugin: BasePlugin() {
     override fun load() {
-        // Mendaftarkan Provider Utama
+        // 1. Daftarkan Provider Utama
         registerMainAPI(LayarKacaProvider())
         
-        // Mendaftarkan Extractor yang SUDAH didefinisikan di Extractors.kt
+        // 2. Daftarkan Extractor Custom Kita (Sesuai nama class di Extractors.kt)
         registerExtractorAPI(Hownetwork())
         registerExtractorAPI(Cloudhownetwork())
         registerExtractorAPI(Co4nxtrl())
         registerExtractorAPI(Furher())
         registerExtractorAPI(Furher2())
         registerExtractorAPI(Turbovidhls())
+        registerExtractorAPI(F16px())
+        registerExtractorAPI(Hydrax()) // AbyssCDN
         
-        // Mendaftarkan Extractor Bawaan Cloudstream (opsional tapi berguna)
+        // 3. Daftarkan Extractor Bawaan Cloudstream (Berguna untuk server umum)
         registerExtractorAPI(VidHidePro6())
     }
 }
