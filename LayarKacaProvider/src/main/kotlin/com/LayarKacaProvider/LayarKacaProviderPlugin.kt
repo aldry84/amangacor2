09bb2003165f2b45f8f09005805ccdb29b75ce5d
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-16 16:37:36 +0900

    Update LayarKacaProviderPlugin.kt

--- LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
+++ LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
@@ -1,53 +1,21 @@
 package com.layarKacaProvider
 
-import com.lagradost.cloudstream3.SubtitleFile
-import com.lagradost.cloudstream3.utils.ExtractorApi
-import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.INFER_TYPE
-import com.lagradost.cloudstream3.utils.Qualities
-import com.lagradost.cloudstream3.utils.newExtractorLink // Import wajib baru
-import com.lagradost.cloudstream3.extractors.Filesim
+import com.lagradost.cloudstream3.extractors.EmturbovidExtractor
+import com.lagradost.cloudstream3.extractors.VidHidePro6
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.BasePlugin
 
-class Co4nxtrl : Filesim() {
-    override val mainUrl = "https://co4nxtrl.com"
-    override val name = "Co4nxtrl"
-    override val requiresReferer = true
-}
-
-class Furher : Filesim() {
-    override val name = "Furher"
-    override var mainUrl = "https://furher.in"
-}
-
-class Turbovidhls : ExtractorApi() {
-    override val name = "Turbovid"
-    override val mainUrl = "https://turbovidhls.com"
-    override val requiresReferer = true
-
-    override suspend fun getUrl(
-        url: String,
-        referer: String?,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
-    ) {
-        val headers = mapOf(
-            "Origin" to "https://turbovidhls.com",
-            "Referer" to "https://turbovidhls.com/",
-            "User-Agent" to "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
-            "Accept" to "*/*"
-        )
-
-        // SOLUSI: Menggunakan newExtractorLink sesuai saran log error
-        callback(
-            newExtractorLink(
-                source = this.name,
-                name = this.name,
-                url = url,
-                referer = "https://turbovidhls.com/",
-                quality = Qualities.Unknown.value,
-                type = INFER_TYPE,
-                headers = headers
-            )
-        )
+@CloudstreamPlugin
+class LayarKacaProviderPlugin: BasePlugin() {
+    override fun load() {
+        // Daftarkan Provider Utama
+        registerMainAPI(LayarKacaProvider())
+        
+        // Daftarkan Extractor (Hanya panggil namanya, jangan buat class di sini!)
+        registerExtractorAPI(EmturbovidExtractor())
+        registerExtractorAPI(Furher())
+        registerExtractorAPI(Turbovidhls())
+        registerExtractorAPI(VidHidePro6())
+        registerExtractorAPI(Co4nxtrl())
     }
 }
