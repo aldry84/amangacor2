e61a0bf5edf852af3bd25a3ee98a9e11e27ca8b9
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-17 02:23:57 +0900

    Update LayarKacaProviderPlugin.kt

--- LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
+++ LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
@@ -2,20 +2,15 @@ package com.layarKacaProvider
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.BasePlugin
-// Hapus import com.lagradost.cloudstream3.extractors.EmturbovidExtractor (yang bikin error)
 import com.lagradost.cloudstream3.extractors.VidHidePro6
 
 @CloudstreamPlugin
 class LayarKacaProviderPlugin: BasePlugin() {
     override fun load() {
-        // Daftarkan Provider Utama
         registerMainAPI(LayarKacaProvider())
         
-        // Daftarkan Extractor Custom Kita
-        // Perhatikan: Kita menghapus EmturbovidExtractor() bawaan
-        // Kita akan menanganinya lewat Turbovidhls dan EmturboCustom di Extractors.kt
-        
-        registerExtractorAPI(EmturboCustom()) // <--- INI BARU
+        // GUNAKAN CUSTOM EXTRACTOR KITA
+        registerExtractorAPI(EmturboCustom()) 
         registerExtractorAPI(Turbovidhls())
         
         registerExtractorAPI(Furher())
