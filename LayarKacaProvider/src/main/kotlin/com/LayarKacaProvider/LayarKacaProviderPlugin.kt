93ed61154fce199609f77fd698e5a617a5a0d75c
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-17 20:32:28 +0900

    Update LayarKacaProviderPlugin.kt

--- LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
+++ LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
@@ -1,6 +1,5 @@
 package com.layarKacaProvider
 
-import com.lagradost.cloudstream3.extractors.EmturbovidExtractor
 import com.lagradost.cloudstream3.extractors.VidHidePro6
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.BasePlugin
@@ -8,13 +7,15 @@ import com.lagradost.cloudstream3.plugins.BasePlugin
 @CloudstreamPlugin
 class LayarKacaProviderPlugin: BasePlugin() {
     override fun load() {
-        // Daftarkan Provider Utama
+        // 1. Daftarkan Provider Utama
         registerMainAPI(LayarKacaProvider())
         
-        // Daftarkan Extractor yang ada di Extractors.kt
-        registerExtractorAPI(EmturbovidExtractor())
+        // 2. Daftarkan Extractor Custom
+        // HAPUS 'EmturbovidExtractor()' agar tidak bentrok dengan Turbovidhls
+        // registerExtractorAPI(EmturbovidExtractor()) 
+        
         registerExtractorAPI(Furher())
-        registerExtractorAPI(Turbovidhls())
+        registerExtractorAPI(Turbovidhls()) // Ini yang akan diprioritaskan sekarang
         registerExtractorAPI(VidHidePro6())
         registerExtractorAPI(Co4nxtrl())
     }
