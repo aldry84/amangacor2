577ba367f2894a3a3c7ecfee4bb972e7c0f810a1
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-20 11:23:30 +0900

    Update build.gradle.kts

--- JavHey/build.gradle.kts
+++ JavHey/build.gradle.kts
@@ -24,7 +24,7 @@ cloudstream {
     )
 
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=tv7.lk21.am.in&sz=%size%"
+    iconUrl = "https://javhey.com/favicon.ico"
 
     isCrossPlatform = true
 }
