4f301e6bdef1eaead57998e7dea1332843143f84
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-20 13:52:31 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,11 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.13.0")
+        // GANTI KE VERSI STABIL
+        classpath("com.android.tools.build:gradle:8.2.2")
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        // GUNAKAN KOTLIN 1.9.23 (Standar Cloudstream)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
     }
 }
 
@@ -45,20 +47,20 @@ subprojects {
 
         defaultConfig {
             minSdk = 21
-            compileSdkVersion(35)
-            targetSdk = 35
-
+            compileSdkVersion(34) // Disarankan 34 untuk stabilitas, tapi 35 boleh juga
+            targetSdk = 34
         }
 
+        // BAGIAN PENTING: WAJIB UPGRADE KE JAVA 11
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
         }
-
 
         tasks.withType<KotlinJvmCompile> {
             compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_1_8)
+                // BAGIAN PENTING: WAJIB JVM 11
+                jvmTarget.set(JvmTarget.JVM_11) 
                 freeCompilerArgs.addAll(
                     "-Xno-call-assertions",
                     "-Xno-param-assertions",
@@ -71,19 +73,23 @@ subprojects {
     dependencies {
         val implementation by configurations
         val cloudstream by configurations
+        
+        // Menggunakan library Cloudstream
         cloudstream("com.lagradost:cloudstream3:pre-release")
 
-        // Other dependencies
+        // Dependencies standar
         implementation(kotlin("stdlib"))
         implementation("com.github.Blatzar:NiceHttp:0.4.13")
-        implementation("org.jsoup:jsoup:1.21.2")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.1")
-        implementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
-        implementation("org.mozilla:rhino:1.8.0")
+        implementation("org.jsoup:jsoup:1.17.2") // Update ke versi stabil umum
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
+        implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+        implementation("org.mozilla:rhino:1.7.14") // Versi yang lebih kompatibel
         implementation("me.xdrop:fuzzywuzzy:1.4.0")
-        implementation("com.google.code.gson:gson:2.13.2")
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+        
+        // Library tambahan yang kamu masukkan
+        implementation("com.google.code.gson:gson:2.10.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
         implementation("app.cash.quickjs:quickjs-android:0.9.2")
         implementation("com.github.vidstige:jadb:v1.2.1")
         implementation("org.bouncycastle:bcpkix-jdk15on:1.70")
