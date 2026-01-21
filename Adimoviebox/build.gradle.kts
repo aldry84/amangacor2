a784e060402ec65f329beb92b7029b7856d0e622
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-22 06:08:14 +0700

    Add files via upload

---
+++ Adimoviebox/build.gradle.kts
@@ -0,0 +1,13 @@
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+
+version = 10 // Versi diperbarui karena perubahan besar pada API
+
+cloudstream {
+    description = "Nonton sampe biji mata kaluar"
+    language    = "id"
+    authors = listOf("AdiManuLateri3")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries", "Anime", "AsianDrama")
+    iconUrl = "https://raw.githubusercontent.com/michat88/Zaneta/refs/heads/main/Icons/adi.png"
+    isCrossPlatform = true
+}
