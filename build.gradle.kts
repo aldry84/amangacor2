570304dba3b0eebbf10ba09964ff28fdf2600c03
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-21 23:32:11 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,12 +12,9 @@ buildscript {
     }
 
     dependencies {
-        // Alat pembangun Android
-        classpath("com.android.tools.build:gradle:8.2.2")
-        // Alat pembangun Cloudstream
+        classpath("com.android.tools.build:gradle:8.13.2")
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        // KOTLIN VERSI BARU (2.1.0) supaya error hilang
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
 
@@ -39,9 +36,8 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        // Ganti link ini dengan link Github kamu kalau sudah punya
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/aldry84/amangacor2")
-        authors = listOf("PartnerCoding") 
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/phisher98/cloudstream-extensions-phisher")
+        authors = listOf("Phisher98")
     }
 
     android {
@@ -49,27 +45,24 @@ subprojects {
 
         defaultConfig {
             minSdk = 21
-            compileSdkVersion(34)
-            targetSdk = 34
+            compileSdkVersion(35)
+            targetSdk = 35
+
         }
 
-        // Kita atur supaya pakai Java 11 (Wajib buat Cloudstream jaman now)
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
         }
+
 
         tasks.withType<KotlinJvmCompile> {
             compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_11)
-                
-                // INI BAGIAN PALING PENTING!
-                // Kode ini memaksa komputer untuk MENGABAIKAN error beda versi
+                jvmTarget.set(JvmTarget.JVM_1_8)
                 freeCompilerArgs.addAll(
                     "-Xno-call-assertions",
                     "-Xno-param-assertions",
-                    "-Xno-receiver-assertions",
-                    "-Xskip-metadata-version-check" 
+                    "-Xno-receiver-assertions"
                 )
             }
         }
@@ -78,22 +71,20 @@ subprojects {
     dependencies {
         val implementation by configurations
         val cloudstream by configurations
-        
-        // Mengambil Cloudstream versi terbaru (Pre-release)
         cloudstream("com.lagradost:cloudstream3:pre-release")
 
-        // Bahan-bahan standar lainnya
+        // Other dependencies
         implementation(kotlin("stdlib"))
-        implementation("com.github.Blatzar:NiceHttp:0.4.13")
-        implementation("org.jsoup:jsoup:1.17.2") 
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
-        implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
-        implementation("org.mozilla:rhino:1.7.14")
+        implementation("com.github.Blatzar:NiceHttp:0.4.16")
+        implementation("org.jsoup:jsoup:1.22.1")
+        implementation("androidx.annotation:annotation:1.9.1")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.1")
+        implementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+        implementation("org.mozilla:rhino:1.9.0")
         implementation("me.xdrop:fuzzywuzzy:1.4.0")
-        implementation("com.google.code.gson:gson:2.10.1")
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-        implementation("app.cash.quickjs:quickjs-android:0.9.2")
+        implementation("com.google.code.gson:gson:2.13.2")
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
         implementation("com.github.vidstige:jadb:v1.2.1")
         implementation("org.bouncycastle:bcpkix-jdk15on:1.70")
     }
@@ -102,4 +93,3 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-
