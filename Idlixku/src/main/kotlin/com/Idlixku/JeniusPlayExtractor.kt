be6f09a33c3fb6ba3b9ded21fc77659cb957acd0
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-23 20:10:57 +0900

    Update JeniusPlayExtractor.kt

--- Idlixku/src/main/kotlin/com/Idlixku/JeniusPlayExtractor.kt
+++ Idlixku/src/main/kotlin/com/Idlixku/JeniusPlayExtractor.kt
@@ -2,8 +2,7 @@ package com.Idlixku
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.* // Import ini WAJIB untuk newExtractorLink
-import com.lagradost.cloudstream3.utils.AppUtils.parseJson
+import com.lagradost.cloudstream3.utils.* import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
 
 class JeniusPlayExtractor : ExtractorApi() {
     override val name = "JeniusPlay"
@@ -21,7 +20,14 @@ class JeniusPlayExtractor : ExtractorApi() {
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ) {
-        val id = url.substringAfter("/video/").substringBefore("/")
+        // PERBAIKAN: Logic ekstraksi ID yang lebih tangguh
+        // Coba ambil dari parameter "data" dulu, kalau tidak ada baru dari path
+        val id = if (url.contains("data=")) {
+            url.substringAfter("data=").substringBefore("&")
+        } else {
+            url.substringAfter("/video/").substringBefore("/")
+        }
+        
         val apiUrl = "$mainUrl/player/index.php?data=$id&do=getVideo"
 
         runCatching {
@@ -35,7 +41,6 @@ class JeniusPlayExtractor : ExtractorApi() {
             
             val videoUrl = response?.securedLink ?: response?.videoSource ?: return@runCatching
 
-            // MENGGUNAKAN NEWEXTRACTORLINK SESUAI PERMINTAAN DAN CONTOH ADIMOVIEBOX
             callback.invoke(
                 newExtractorLink(
                     name,
