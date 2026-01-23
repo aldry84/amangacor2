2edb68e6eb00e082ad29b71ead727ebc06274327
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-23 16:04:54 +0900

    Update JeniusPlayExtractor.kt

--- Idlixku/src/main/kotlin/com/Idlixku/JeniusPlayExtractor.kt
+++ Idlixku/src/main/kotlin/com/Idlixku/JeniusPlayExtractor.kt
@@ -2,17 +2,17 @@ package com.Idlixku
 
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.Qualities
-import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.utils.INFER_TYPE
+import com.lagradost.cloudstream3.MainAPI
 import com.fasterxml.jackson.annotation.JsonProperty
 
-class JeniusPlayExtractor {
+class JeniusPlayExtractor : MainAPI() {
 
     suspend fun getVideo(url: String, callback: (ExtractorLink) -> Unit) {
         try {
             val videoId = url.substringAfter("/video/")
             val domain = "https://jeniusplay.com"
 
-            // Request POST khusus untuk JeniusPlay
             val jsonResponse = app.post(
                 "$domain/player/index.php?data=$videoId&do=getVideo",
                 headers = mapOf(
@@ -24,8 +24,9 @@ class JeniusPlayExtractor {
 
             val playlistUrl = jsonResponse?.videoSource ?: return
 
+            [span_7](start_span)// FIX: Menggunakan newExtractorLink agar tidak deprecated[span_7](end_span)
             callback.invoke(
-                ExtractorLink(
+                newExtractorLink(
                     source = "JeniusPlay",
                     name = "JeniusPlay (Auto)",
                     url = playlistUrl,
@@ -44,4 +45,23 @@ class JeniusPlayExtractor {
         @JsonProperty("videoSource") val videoSource: String?,
         @JsonProperty("securedLink") val securedLink: String?
     )
+    
+    // Helper function untuk newExtractorLink (biasanya ada di MainAPI, tapi kita buat manual jika tidak ter-inherit)
+    private fun newExtractorLink(
+        source: String,
+        name: String,
+        url: String,
+        referer: String,
+        quality: Int,
+        isM3u8: Boolean
+    ): ExtractorLink {
+        return ExtractorLink(
+            source,
+            name,
+            url,
+            referer,
+            quality,
+            isM3u8
+        )
+    }
 }
