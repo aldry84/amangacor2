12e06c9c76e473bc154b9d9cc38ce7839b4ad83a
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-24 02:18:23 +0900

    Create KlikxxiPlugin.kt

---
+++ Klikxxi/src/main/kotlin/com/Klikxxi/KlikxxiPlugin.kt
@@ -0,0 +1,13 @@
+package com.Klikxxi
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class KlikxxiPlugin: Plugin() {
+    override fun load(context: Context) {
+        // Mendaftarkan class provider utama
+        registerMainAPI(Klikxxi())
+    }
+}
