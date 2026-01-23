125d78ea2e21581da4ede5bd7a7a933474cfd00b
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-23 21:33:54 +0900

    Update JeniusPlayExtractor.kt

--- Idlixku/src/main/kotlin/com/Idlixku/JeniusPlayExtractor.kt
+++ Idlixku/src/main/kotlin/com/Idlixku/JeniusPlayExtractor.kt
@@ -1,16 +1,24 @@
 package com.Idlixku
 
+// Import tetap dipertahankan secara eksplisit sesuai permintaanmu
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.*
-import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
+import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.newSubtitleFile
+import com.lagradost.cloudstream3.utils.AppUtils
+import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.ExtractorLinkType
+import com.lagradost.cloudstream3.utils.getAndUnpack
+import com.lagradost.cloudstream3.utils.newExtractorLink
+import com.lagradost.cloudstream3.utils.Qualities
 
-class JeniusPlayExtractor : ExtractorApi() {
+class Jeniusplay : ExtractorApi() {
     override var name = "Jeniusplay"
     override var mainUrl = "https://jeniusplay.com"
     override val requiresReferer = true
 
-    // 1. PINDAHKAN DATA CLASS DARI HEXATED KE SINI (Agar tidak perlu import luar)
+    // DATA CLASS PINDAHAN DARI HEXATED (Sekarang milik Idlixku)
     data class ResponseSource(
         @param:JsonProperty("videoSource") val videoSource: String? = null,
         @param:JsonProperty("securedLink") val securedLink: String? = null
@@ -18,8 +26,7 @@ class JeniusPlayExtractor : ExtractorApi() {
 
     data class Tracks(
         @param:JsonProperty("file") val file: String,
-        @param:JsonProperty("label") val label: String? = null,
-        @param:JsonProperty("kind") val kind: String? = null
+        @param:JsonProperty("label") val label: String? = null
     )
 
     override suspend fun getUrl(
@@ -28,19 +35,16 @@ class JeniusPlayExtractor : ExtractorApi() {
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ) {
-        val document = app.get(url, referer = referer).document
+        val res = app.get(url, referer = referer)
+        val document = res.document
         
-        // Ambil Hash ID
-        val hash = if (url.contains("data=")) {
-            url.substringAfter("data=").substringBefore("&")
-        } else {
-            url.split("/").last()
-        }
+        // Mengambil Hash ID dari URL
+        val hash = url.substringAfter("data=").substringBefore("&")
 
-        // Request Link Video (M3U8)
+        // Request Video Source menggunakan POST (Metode Hexated)
         val response = app.post(
             url = "$mainUrl/player/index.php?data=$hash&do=getVideo",
-            data = mapOf("hash" to hash, "r" to (referer ?: "https://tv12.idlixku.com/")),
+            data = mapOf("hash" to hash, "r" to (referer ?: "")),
             referer = url,
             headers = mapOf("X-Requested-With" to "XMLHttpRequest")
         ).parsedSafe<ResponseSource>()
@@ -48,7 +52,7 @@ class JeniusPlayExtractor : ExtractorApi() {
         val m3uLink = response?.securedLink ?: response?.videoSource
 
         if (!m3uLink.isNullOrEmpty()) {
-            // Gunakan format newExtractorLink yang kita tahu berhasil di compiler kamu
+            // Menggunakan format 4 parameter + lambda agar lulus compile
             callback.invoke(
                 newExtractorLink(
                     name,
@@ -57,32 +61,26 @@ class JeniusPlayExtractor : ExtractorApi() {
                     ExtractorLinkType.M3U8
                 ) {
                     this.referer = url
+                    this.quality = Qualities.Unknown.value
                 }
             )
         }
 
-        // --- LOGIKA SUBTITLE (UNPACKER) ---
-        // Mencari subtitle di dalam script yang ter-enkripsi (P.A.C.K.E.R)
+        // Logic Subtitle menggunakan Unpacker (Hexated Style)
         document.select("script").forEach { script ->
             val scriptData = script.data()
             if (scriptData.contains("eval(function(p,a,c,k,e,d)")) {
-                try {
-                    // Unpack script JS
-                    val unpacked = JsUnpacker.unpackAndGet(scriptData) ?: ""
-                    if (unpacked.contains("\"tracks\":[")) {
-                        val subData = unpacked.substringAfter("\"tracks\":[").substringBefore("],")
-                        // Parse JSON Subtitle
-                        tryParseJson<List<Tracks>>("[$subData]")?.forEach { subtitle ->
-                            subtitleCallback.invoke(
-                                newSubtitleFile(
-                                    getLanguage(subtitle.label ?: ""),
-                                    subtitle.file
-                                )
+                val unpacked = getAndUnpack(scriptData)
+                if (unpacked.contains("\"tracks\":[")) {
+                    val subData = unpacked.substringAfter("\"tracks\":[").substringBefore("],")
+                    AppUtils.tryParseJson<List<Tracks>>("[$subData]")?.forEach { subtitle ->
+                        subtitleCallback.invoke(
+                            newSubtitleFile(
+                                getLanguage(subtitle.label ?: ""),
+                                subtitle.file
                             )
-                        }
+                        )
                     }
-                } catch (e: Exception) {
-                    e.printStackTrace()
                 }
             }
         }
