6570cfdb94d80b8718c36355781c16bba063ba26
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-24 05:49:45 +0900

    Create NgeFilmPlugin.kt

---
+++ NgeFilm/src/main/kotlin/com/NgeFilm/NgeFilmPlugin.kt
@@ -0,0 +1,13 @@
+package com.NgeFilm
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class NgeFilmPlugin: Plugin() {
+    override fun load(context: Context) {
+        // Mendaftarkan provider utama kita
+        registerMainAPI(NgeFilm())
+    }
+}
