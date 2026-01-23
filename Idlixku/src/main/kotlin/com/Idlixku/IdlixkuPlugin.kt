07c65dda495225ade088f81fab949ff7918468d5
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-23 15:33:36 +0900

    Create IdlixkuPlugin.kt

---
+++ Idlixku/src/main/kotlin/com/Idlixku/IdlixkuPlugin.kt
@@ -0,0 +1,13 @@
+package com.Idlixku
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class IdlixkuPlugin : Plugin() {
+    override fun load(context: Context) {
+        // Mendaftarkan Provider Utama
+        registerMainAPI(Idlixku())
+    }
+}
