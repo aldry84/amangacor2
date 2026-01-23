97273573aa955c4af8c3c41e4021f0a45de80717
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-23 21:53:48 +0900

    Update IdlixkuPlugin.kt

--- Idlixku/src/main/kotlin/com/Idlixku/IdlixkuPlugin.kt
+++ Idlixku/src/main/kotlin/com/Idlixku/IdlixkuPlugin.kt
@@ -6,6 +6,11 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 @CloudstreamPlugin
 class IdlixkuPlugin: BasePlugin() {
     override fun load() {
+        // Mendaftarkan Provider utama (untuk scraping daftar film & search)
         registerMainAPI(IdlixkuProvider())
+        
+        // Mendaftarkan Extractor (untuk mengolah link player JeniusPlay)
+        // TANPA INI, LINK VIDEO TIDAK AKAN PERNAH JALAN!
+        registerExtractorAPI(JeniusPlayExtractor())
     }
 }
