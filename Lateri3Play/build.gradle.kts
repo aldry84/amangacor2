26416d4f9339dba587083c848ef004b3c8137a5f
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-26 00:48:57 +0900

    Update build.gradle.kts

--- Lateri3Play/build.gradle.kts
+++ Lateri3Play/build.gradle.kts
@@ -1,48 +1,27 @@
-@file:Suppress("UnstableApiUsage")
-
-android {
-    // Cukup sisakan namespace saja.
-    // compileSdk, minSdk, kotlinOptions, dll sudah diatur otomatis oleh Root build.gradle.kts
-    namespace = "com.AdiManuLateri3"
-}
+// use an integer for version numbers
+version = 179
 
 cloudstream {
-    // 'name' dihapus karena read-only. Nama plugin diambil dari nama class utama.
     language = "id"
-    description = "Lateri3Play Provider - Multi Sources"
-    authors = listOf("AdiManu")
-    
+    // All of these properties are optional, you can safely remove them
+
+     description = "Nonton awas jang sampe buta kawan"
+     authors = listOf("AdiManuLateri3", "Trinity")
+
     /**
-     * Status int:
+     * Status int as the following:
      * 0: Down
      * 1: Ok
      * 2: Slow
      * 3: Beta only
-     */
-    status = 3 
-
+     * */
+    status = 1 // will be 3 if unspecified
     tvTypes = listOf(
+        "AsianDrama",
         "TvSeries",
-        "Movie",
         "Anime",
-        "AsianDrama"
+        "Movie",
     )
 
-    // Icon URL
     iconUrl = "https://raw.githubusercontent.com/michat88/Zaneta/refs/heads/main/Icons/adi.png"
-
-    requiresResources = true
-}
-
-dependencies {
-    val cloudstream by configurations
-    
-    // Dependencies Standar
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.browser:browser:1.8.0")
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    
-    // Core CloudStream
-    cloudstream("com.lagradost:cloudstream3:pre-release")
 }
