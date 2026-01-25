60d06d546e61f8e29390b2530af2d8047eb0a9e9
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-25 13:25:52 +0900

    Update KisskhProviderPlugin.kt

--- KisskhProvider/src/main/kotlin/com/Phisher98/KisskhProviderPlugin.kt
+++ KisskhProvider/src/main/kotlin/com/Phisher98/KisskhProviderPlugin.kt
@@ -1,12 +1,11 @@
-package com.Phisher98 // Pastikan package ini SAMA dengan KisskhProvider.kt
+package com.phisher98
 
-import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.BasePlugin
 
 @CloudstreamPlugin
 class KisskhProviderPlugin: BasePlugin() {
     override fun load() {
-        // Pastikan nama kelasnya benar: KisskhProvider
         registerMainAPI(KisskhProvider())
     }
 }
