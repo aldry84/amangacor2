4990f07fae70c2dafe47a7ffd1e3ad81bfdc06c6
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-01-27 20:29:46 +0900

    Update Extractors.kt

--- NgeFilm/src/main/kotlin/com/NgeFilm/Extractors.kt
+++ NgeFilm/src/main/kotlin/com/NgeFilm/Extractors.kt
@@ -3,6 +3,7 @@ package com.NgeFilm
 import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.base64DecodeArray
+import com.lagradost.cloudstream3.newExtractorLink
 import javax.crypto.Cipher
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
@@ -12,30 +13,33 @@ class RpmLive : ExtractorApi() {
     override val mainUrl = "https://rpmlive.online"
     override val requiresReferer = true
 
+    // Byte Array dari spesifikasi user
     private val key = byteArrayOf(107, 105, 101, 109, 116, 105, 101, 110, 109, 117, 97, 57, 49, 49, 99, 97)
     private val iv = byteArrayOf(49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 111, 105, 117, 121, 116, 114)
 
     suspend fun getStreamUrl(url: String, callback: (ExtractorLink) -> Unit) {
         val response = app.get(url, referer = "https://ngefilm21.pw/").text
+        
+        // Ambil data terenkripsi dari makePlayer("...")
         val encryptedData = Regex("""makePlayer\("([^"]+)""").find(response)?.groupValues?.get(1)
 
         encryptedData?.let {
             val decrypted = decryptAes(it)
+            
+            // Ambil URL dari JSON hasil dekripsi
             val videoUrl = Regex("""file":"([^"]+)""").find(decrypted)?.groupValues?.get(1)
             
             videoUrl?.let { link ->
-                val cleanLink = link.replace("\\", "")
+                val cleanedLink = link.replace("\\", "")
                 
-                // Menerapkan pola newExtractorLink seperti pada KisskhProvider
+                // FIX: Menggunakan newExtractorLink ala KisskhProvider
                 callback.invoke(
-                    newExtractorLink(
-                        name = this.name,
-                        source = this.name,
-                        url = cleanLink,
-                        referer = url,
-                        quality = Qualities.P720.value,
-                        [span_1](start_span)type = if (cleanLink.contains("m3u8")) INFER_TYPE else IntentMedia.VIDEO // Mirip logika di Kisskh[span_1](end_span)
-                    )
+                    newExtractorLink(this.name, this.name, cleanedLink, INFER_TYPE) {
+                        this.referer = url
+                        this.quality = Qualities.P720.value
+                        // Kita bisa set isM3u8 secara manual jika link mengandung m3u8
+                        this.isM3u8 = cleanedLink.contains("m3u8")
+                    }
                 )
             }
         }
