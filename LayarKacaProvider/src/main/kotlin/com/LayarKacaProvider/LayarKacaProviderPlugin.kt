4d13c49d4891dd7309826b32ffc8943368b0d9cd
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-02-07 22:37:29 +0900

    Update LayarKacaProviderPlugin.kt

--- LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
+++ LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
@@ -1,22 +1,13 @@
-package com.layarKacaProvider
+package com.LayarKacaProvider
 
-import com.lagradost.cloudstream3.extractors.EmturbovidExtractor
-import com.lagradost.cloudstream3.extractors.VidHidePro6
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
 
 @CloudstreamPlugin
-class LayarKacaProviderPlugin: BasePlugin() {
-    override fun load() {
-        // All providers should be added in this manner. Please don't edit the providers list directly.
+class LayarKacaPlugin : Plugin() {
+    override fun load(context: Context) {
+        // Mendaftarkan Provider kita ke aplikasi
         registerMainAPI(LayarKacaProvider())
-        registerExtractorAPI(EmturbovidExtractor())
-        registerExtractorAPI(Furher())
-        registerExtractorAPI(Hownetwork())
-        registerExtractorAPI(VidHidePro6())
-        registerExtractorAPI(Furher2())
-        registerExtractorAPI(Turbovidhls())
-        registerExtractorAPI(Cloudhownetwork())
-        registerExtractorAPI(Co4nxtrl())
     }
 }
