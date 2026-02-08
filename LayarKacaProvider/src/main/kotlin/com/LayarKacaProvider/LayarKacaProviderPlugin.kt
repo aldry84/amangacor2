3244ea577e59425cfda884639ce3e2f81f538118
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-02-08 21:09:50 +0900

    Update LayarKacaProviderPlugin.kt

--- LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
+++ LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
@@ -1,19 +1,17 @@
 package com.LayarKacaProvider
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
-@Plugin
+// Hapus anotasi @Plugin, biarkan class ini polos.
+// Cloudstream akan menemukannya lewat manifest.json
 class LayarKacaProviderPlugin : CloudstreamPlugin() {
+    
     override fun load(context: Context) {
-        // 1. Mendaftarkan Provider Utama (LayarKaca21)
-        // Ini agar muncul di halaman Home aplikasi
+        // Mendaftarkan Provider
         registerMainAPI(LayarKacaProvider())
 
-        // 2. Mendaftarkan Extractor Khusus (Hydrax)
-        // Ini penting agar saat aplikasi menemukan link "abysscdn.com", 
-        // dia tahu harus menggunakan logika di file Hydrax.kt kita
+        // Mendaftarkan Extractor
         registerExtractorAPI(Hydrax())
     }
 }
