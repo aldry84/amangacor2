aef5e4226e34c6a5f570f535ca300576499e3ace
aldry84 <aldrymanusiwa.aman@gmail.com>
2026-02-08 10:54:59 +0900

    Update LayarKacaProviderPlugin.kt

--- LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
+++ LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
@@ -11,9 +11,6 @@ class LayarKacaPlugin : Plugin() {
         registerMainAPI(LayarKacaProvider())
         
         // Extractor Servers
-        registerExtractorAPI(EmturbovidExtractor()) // TurboVip
-        registerExtractorAPI(P2PExtractor())        // P2P
-        registerExtractorAPI(F16Extractor())        // CAST (Baru!)
         registerExtractorAPI(HydraxExtractor())
     }
 }
